97958305700de4acee5cd760117445bc313fcd29
M.Mohsen <eng.mmsayed@gmail.com>
2020-03-12 18:50:47 +0200

    Bump version to '0.2'

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 os.chdir(os.path.abspath(os.path.dirname(__file__)))
 
 __PKGNAME__ = 'exonwebui'
-__VERSION__ = '0.1.dev'
+__VERSION__ = '0.2.dev'
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
