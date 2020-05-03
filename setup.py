e7993045f366de8248a37862f1753e4ac2611c15
M.Mohsen <eng.mmsayed@gmail.com>
2020-05-03 02:46:59 +0200

    Release version '0.4'

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 os.chdir(os.path.abspath(os.path.dirname(__file__)))
 
 __PKGNAME__ = 'exonwebui'
-__VERSION__ = '0.4.dev'
+__VERSION__ = '0.4'
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
