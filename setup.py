51c976db12c5b1c2df5f68f9e4b3199fb808a05b
M.Mohsen <mmsayed.dev@gmail.com>
2022-11-09 15:55:44 +0200

    adjust exonutils version requirement

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name="exonwebui",
     install_requires=[
-        'exonutils>=6.2,<7.0',
+        'exonutils>=6.3,<7.0',
         'flask>=2.2',
         'Jinja2>=3.1',
         'Babel>=2.10',
