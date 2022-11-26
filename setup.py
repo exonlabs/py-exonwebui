9a1922e7d0193a34bee476af21a843da575ddc1d
M.Mohsen <mmsayed.dev@gmail.com>
2022-11-26 17:55:14 +0200

    update exonutils and packages required versions

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from setuptools import setup
 setup(
     name="exonwebui",
     install_requires=[
-        'exonutils>=6.3,<7.0',
+        'exonutils>=6.5,<7.0',
         'flask>=2.2',
         'Jinja2>=3.1',
-        'Babel>=2.10',
+        'Babel>=2.11',
         'flask-babelex>=0.9',
     ],
 )
