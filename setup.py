e111fcb6afdca848928076317cc657d3225ed949
Hassan Zaalouk <>
2024-02-25 14:07:35 +0200

    Adjust Package versions

--- setup.py
+++ setup.py
@@ -7,6 +7,9 @@ os.chdir(os.path.abspath(os.path.dirname(__file__)))
 setup(
     name="exonwebui",
     install_requires=[
+        'flask>=2.0,<3.0',
+        'Werkzeug>=2.0,<3.0',
         'exonutils>=7.0,<8.0',
+        'pytz',
     ],
 )
